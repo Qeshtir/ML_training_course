101f113af6e9be9d0239c40066d072f074c56727
Igor Makarov <realneiro@gmail.com>
2024-03-08 22:38:47 +0300

    This is a final project part 2 - best personal solution
    Paths fixed. Better use full paths in future

--- 00_Final_Call/app_fc_2/app_fc_2_sep.py
+++ 00_Final_Call/app_fc_2/app_fc_2_sep.py
@@ -30,7 +30,7 @@ def get_model_path(path: str) -> str:
 
 
 def load_models():
-    model_path = get_model_path("/00_Final_Call/app_fc_2/cbm_best.cbm")
+    model_path = get_model_path("cbm_best.cbm")
     from_file = CatBoostClassifier()
     model = from_file.load_model(model_path, format='cbm')
     return model
